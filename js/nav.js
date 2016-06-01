1358b802c785215e9704b25a55a0cbcc60b9325d
junge1483797 <1041607847@qq.com>
2016-06-01 16:07:17 +0800

    1.0.2

--- js/nav.js
+++ js/nav.js
@@ -2,14 +2,17 @@ $(".nav1").on("click","li",function(){
    	$(this).addClass("liColor").siblings().removeClass("liColor");
    	});
 $(".block").on("click",function(){
-   	if($(document).width() <= 1000){
-   		$(".menu").fadeToggle();
-   	}else{
-   				
-   	}
+   		$(".menu").fadeToggle();	
 });
 
 $(".goTop").on("click",function(){
    			$("body,html").scrollTop(0);
-// 			console.log("1")
 });
+
+$(window).resize(function() {
+	if($(document).width() >= 1000) {
+		$(".menu").show();
+	}else {
+		$(".menu").hide();
+	}
+})
